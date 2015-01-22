f7dde117fb9a340e40008d21aab213e636ea38f7
Dane Lipscombe <dane.lipscombe@mytrax.co.jp>
2015-01-22 11:42:46 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+var imagePath = path.resolve(__dirname, '../images'),
+    spriteOutput = imagePath;
+
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+              test: /\.scss$/,
+              // Query parameters are passed to node-sass
+              loader: "style!css!compass?outputStyle=expanded&" +
+                "includePaths[]=" +
+                  (path.resolve(__dirname, "./bower_components")) + "&" +
+                "includePaths[]=" +
+                  (path.resolve(__dirname, "./node_modules")) + "&" +
+                  "imagePath=" + imagePath + "&" +
+                  "spriteOutput=" + spriteOutput
+            }
+        ]
+    }
+};
